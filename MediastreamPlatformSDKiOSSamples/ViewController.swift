8b9161e577790479747c813a12d771ceab3f97f1
cruiz666 <ruizcarlos1985@gmail.com>
2020-07-10 18:18:59 -0400

    Initial Commit

---
+++ MediastreamPlatformSDKiOSSamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediastreamPlatformSDKiOSSamples
+//
+//  Created by Carlos Ruiz on 10-07-20.
+//  Copyright © 2020 Carlos Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
